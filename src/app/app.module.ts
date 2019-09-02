5823c933fa509be522402f3f870d4096503c6415
JLGallardoV <Gallardo.vaca@gmail.com>
2019-09-02 16:22:30 -0500

    GRID Y ESTRUCTURA DEL MENU PRINCIPAL CREADAS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -12,6 +16,10 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
     AppRoutingModule
   ],
   providers: [],
