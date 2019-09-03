fd981e73a17f03477d9d778fd3984aa791dd5c81
JLGallardoV <Gallardo.vaca@gmail.com>
2019-09-03 17:33:32 -0500

    GENEREACION MODULOS PROVEEDORES

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,13 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { ProveedoresComponent } from './proveedores/proveedores.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    ProveedoresComponent
   ],
   imports: [
     BrowserModule,
