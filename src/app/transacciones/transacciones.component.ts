ef5beb7dadf2a8bb18f8ea778ac88a3325fe1015
JLGallardoV <Gallardo.vaca@gmail.com>
2019-11-06 13:27:39 -0600

    GUI TRANSACCIONES - CONCLUIDO
    - Contiene 1 tab con 2 secciones
    	- Ventas y Compras:
    		- Tabla con paginador
    		- Input buscador funcional
    - Falta un poco de responsividad en el paginador para resoluciones muy pequeñas

--- src/app/transacciones/transacciones.component.ts
+++ src/app/transacciones/transacciones.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet';
 import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap'; //LIBRERIA BOOTSTRAP
 import { FormBuilder,FormGroup,Validators } from '@angular/forms';
-import {ITransacciones, APIService} from '../api.service';
+import {ITransacciones,ICompras,APIService} from '../api.service';
+
 
 @Component({
   selector: 'app-transacciones',
@@ -18,8 +19,11 @@ export class TransaccionesComponent implements OnInit {
 
   //propiedades para la tabla
   displayedColumnsTransacciones: string[] = ['idTransaccion', 'nombreProducto', 'montoIvaTransaccion', 'pagoTransaccion', 'cambioTransaccion','fechaTransaccion','numeroProductosEnTransaccion','nombreVendedor','nombreCliente','tipoPago'];
+  displayedColumnsCompras: string[] = ['idCompra', 'nombreProducto', 'numeroProductosEnCompra', 'montoCompra', 'fechaCompra','nombreProveedor','nombreUsuario'];
   dsTransacciones : MatTableDataSource<ITransacciones>;
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  dsCompras : MatTableDataSource<ICompras>;
+  @ViewChild('MatPaginatorCompras',{static: true})paginatorCompras: MatPaginator;
+  @ViewChild('MatPaginatorTransacciones', {static: true}) paginatorTransacciones: MatPaginator;
 
   constructor(
     private _bottomSheet: MatBottomSheet,
@@ -39,23 +43,59 @@ export class TransaccionesComponent implements OnInit {
     this.titulo = "Agregar Proveedor";
   }
 
+
+  /*
+    la primera vez que cargue su componente, el MatPaginator de la pestaña oculta no se renderiza,
+    por lo que no está definido y no se puede vincular a la fuente de datos.
+
+  */
+
   //LISTAR LAS TRANSACCIONES
   public listarTransacciones(){
     this.API.mostrarTransacciones().subscribe(
       (success:any)=>{
         this.dsTransacciones = new MatTableDataSource(success.respuesta);
+        if(!this.dsTransacciones.paginator){
+          this.dsTransacciones.paginator = this.paginatorTransacciones;
+
+        }
         console.log(this.dsTransacciones);
-        this.dsTransacciones.paginator = this.paginator;
       },
       (error)=>{
         console.log(error);
-
       }
     );
   }
 
+  //LISTAR COMPRAS
+  public listarCompras(){
+    this.API.mostrarCompras().subscribe(
+      (success:any)=>{
+        this.dsCompras = new MatTableDataSource(success.respuesta);
+        if(!this.dsCompras.paginator){
+          this.dsCompras.paginator = this.paginatorCompras;
+        }
+        console.log(this.dsCompras);
+      },
+      (error)=>{
+        console.log(error);
+      }
+    );
+  }
+
+  //FUNCIONALIDAD FILTRAR
+  public filtrarRegistros(filterValue: string) {
+    this.dsTransacciones.filter = filterValue.trim().toLowerCase();
+    this.dsCompras.filter = filterValue.trim().toLowerCase();
+    //si se usa el modulo tab de transacciones, entonces arroja los resultados buscados en la primer pagina: (if reducido)
+    this.dsTransacciones.paginator ? this.dsTransacciones.paginator.firstPage(): null;
+    //si se usa el modulo tab de compras, entonces arroja los resultados buscados en la primer pagina:
+    this.dsCompras.paginator ? this.dsCompras.paginator.firstPage(): null;
+
+  }
   ngOnInit() {
     this.listarTransacciones();
+    this.listarCompras();
   }
 
 }
