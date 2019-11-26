9b1d84defcf6aff8651db434b700815f87bc5e00
JLGallardoV <Gallardo.vaca@gmail.com>
2019-11-26 14:56:17 -0600

    GUI TRANSACCIONES V2 - PROCESO
    - Se ha cambiado a español el paginador.
    - Se ha cambiado el color de los botones de informacion
    - Se ha cambiado el color del modal y se agrego animacion al mismo
    - Todo lo anterior se aplico al tab completo (ventas y compras)

--- src/app/transacciones/transacciones.component.ts
+++ src/app/transacciones/transacciones.component.ts
@@ -2,9 +2,29 @@ import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap'; //LIBRERIA BOOTSTRAP
-import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 import {ITransacciones,ICompras,APIService} from '../api.service';
 import {LoginJwtService} from '../login-jwt.service';
+
+/*ESTA FUNCION UNICAMENTE ES PARA CAMBIAR EL "OF" DEL PAGINADOR A "DE" Y NO SE VEA FEO MEZCLADO EL ESPAÑOL CON INGLES,
+ESTAMOS CONFIGURANDO LOS RANGOS DEL PAGINADOR - CORTESÍA: https://stackblitz.com/edit/angular-5mgfxh-6mbpdq */
+
+const etiquetaRango = (page: number, pageSize: number, length: number) => {
+  if (length == 0 || pageSize == 0) { //caso paginador vacio
+    return `0 de ${length}`;
+  }
+  length = Math.max(length, 0);
+
+  const startIndex = page * pageSize; //indice de inicio
+
+/*if resumido; si el indice de inicio excede la logitud de la lista (6 - 5 de 6 por ejemplo) se veria: 6 - 10 de 6 gracias al
+[pageSizeOptions] lo cual es incorrecto pues solo hay 6 elementos en tal rango ENTONCES mejor coloca como indice final el indice inicial
+quedaria 6 - 6 de 6 que es lo correcto).*/
+  const endIndex = startIndex < length ?
+      Math.min(startIndex + pageSize, length) :
+      startIndex + pageSize;
+
+  return `${startIndex + 1} - ${endIndex} de ${length}`;
+}
 
 
 @Component({
@@ -12,6 +32,8 @@ import {LoginJwtService} from '../login-jwt.service';
   templateUrl: './transacciones.component.html',
   styleUrls: ['./transacciones.component.scss']
 })
+
+
 export class TransaccionesComponent implements OnInit {
   public closeResult: string; //modal
   public modal: NgbModalRef; //modal
@@ -28,7 +50,6 @@ export class TransaccionesComponent implements OnInit {
   constructor(
     public guardian:LoginJwtService,
     private modalService: NgbModal,
-    public formBuilder: FormBuilder,
     public API: APIService
   ) {}
 
@@ -53,6 +74,8 @@ export class TransaccionesComponent implements OnInit {
         this.dsTransacciones = new MatTableDataSource(success.respuesta);
         if(!this.dsTransacciones.paginator){
           this.dsTransacciones.paginator = this.paginatorTransacciones;
+          this.dsTransacciones.paginator._intl.itemsPerPageLabel = 'items por pagina';
+          this.dsTransacciones.paginator._intl.getRangeLabel = etiquetaRango;
 
         }
         console.log(this.dsTransacciones);
